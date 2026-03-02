1c7420f5013d71aadb8b96f3dc8731eca10a19ff
adipro <peter.fedor@gmail.com>
2026-03-02 13:18:28 +0000

    loras

--- download_Flux2Klein9B_LORAs.sh
+++ download_Flux2Klein9B_LORAs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-wget -O /ComfyUI/models/loras/my_loraKlein9bNoFace.safetensors "https://civitai.com/api/download/models/2645238?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
-wget -O /ComfyUI/models/loras/my_loralein9b_nsfw.safetensors "https://civitai.com/api/download/models/2481510?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
+wget -O /ComfyUI/models/loras/my_loraKlein9bNoFace.safetensors "https://civitai.com/api/download/models/2677698?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
+
+#trigger: pussy
+#types bush pussy,pussy with lips butterfly, hairy pussy , shaved pussy,pink pussy, pussy with dark lips, showing her ass from the back 0.7-1.5
+wget -O /ComfyUI/models/loras/my_loralein9bPussy.safetensors "https://civitai.com/api/download/models/2645238?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
