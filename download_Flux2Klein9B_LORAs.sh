cb2ae94f7c39ffdbf8327131269eabbc2f71e725
adipro <peter.fedor@gmail.com>
2026-03-02 15:06:02 +0000

    spreading

--- download_Flux2Klein9B_LORAs.sh
+++ download_Flux2Klein9B_LORAs.sh
@@ -6,3 +6,6 @@ wget -O /ComfyUI/models/loras/my_loraKlein9bNoFace.safetensors "https://civitai.
 #types bush pussy,pussy with lips butterfly, hairy pussy , shaved pussy,pink pussy, pussy with dark lips, showing her ass from the back 0.7-1.5
 wget -O /ComfyUI/models/loras/my_loralein9bPussy.safetensors "https://civitai.com/api/download/models/2645238?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
+#A woman spreading legs
+wget -O /ComfyUI/models/loras/my_loraKlein9bSpreading.safetensors "https://civitai.com/api/download/models/2612784?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
+
