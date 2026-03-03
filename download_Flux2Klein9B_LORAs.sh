ac4b977a80b333520657c59095c3e04fef88bf2c
adipro <peter.fedor@gmail.com>
2026-03-03 18:54:36 +0000

    updates

--- download_Flux2Klein9B_LORAs.sh
+++ download_Flux2Klein9B_LORAs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# good results 
 wget -O /ComfyUI/models/loras/my_loraKlein9bNoFace.safetensors "https://civitai.com/api/download/models/2677698?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
-#trigger: pussy
+#trigger: pussy ( best results with close up )
 #types bush pussy,pussy with lips butterfly, hairy pussy , shaved pussy,pink pussy, pussy with dark lips, showing her ass from the back 0.7-1.5
 wget -O /ComfyUI/models/loras/my_loralein9bPussy.safetensors "https://civitai.com/api/download/models/2645238?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
@@ -10,9 +11,10 @@ wget -O /ComfyUI/models/loras/my_loralein9bPussy.safetensors "https://civitai.co
 wget -O /ComfyUI/models/loras/my_loraKlein9bSpreading.safetensors "https://civitai.com/api/download/models/2612784?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
 #showing in clothes
+# great view from behind . She is on all fours
 wget -O /ComfyUI/models/loras/my_loraKlein9bClothes.safetensors "https://civitai.com/api/download/models/2715365?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
-#showing in clothes
-wget -O /ComfyUI/models/loras/my_loraKlein9bAll.safetensors "https://civitai.com/api/download/models/2474488?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
+#all ( great front view , spreading , on all fours write buttock., dildo )
+wget -O /ComfyUI/models/loras/my_loraKlein9bAll.safetensors "https://civitai.com/api/download/models/2695876?type=Model&format=SafeTensor&token=${CIVIT_AI_TOKEN}"
 
 
--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 git pull
 git config --global user.email "peter.fedor@gmail.com"
 
-cp /workspace/git/ComfyUI_with_Flux/* /ComfyUI/user/default/workflows/
+cp /workspace/git/ComfyUI_with_Flux/comfyui-without-flux/workflows/* /ComfyUI/user/default/workflows/
 bash download_Flux2Klein9B_LORAs.sh
 bash copyFlux2Klein9BToComfy.sh
 
